afa93da6250f8ea831f2b79da82bf67cec101e00
David Cedeño <davidcedeno@MBP-de-David.technicolor.net>
2020-05-14 13:44:28 +0200

    pa arriba menu

---
+++ src/components/MenuLateral.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class MenuLateral extends Component {
+    render() {
+        return (
+
+            <div className="menu-lateral">
+
+            <img src="public/logo192.png" alt=""></img> 
+
+            <div class="ui vertical pointing menu">
+                <a class="active item" href="/pizzas/add">
+                    Crear pizza
+                    </a>
+                <a class="item" href="/pizzas">
+                    Nuestras pizzas
+                    </a>
+                <a class="item" href="/pizza/1/">
+                    Ver pizza
+                    </a>
+            </div>
+
+        </div>
+
+        )
+    }
+}
+
+export default MenuLateral
--- src/pages/ListaPizzasPage.js
+++ src/pages/ListaPizzasPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import MenuLateralPruebas from '../components/MenuLateralPruebas'
+import MenuLateral from '../components/MenuLateral'
 import ListaPizzas from '../components/ListaPizzas'
 import './ListaPizzasPage.css'
 
 const ListaPizzasPage = () => {
     return (
         <div className="lista-pizzas-page">
-            <MenuLateralPruebas></MenuLateralPruebas>
+            <MenuLateral></MenuLateral>
             <ListaPizzas></ListaPizzas>
         </div>
     )
