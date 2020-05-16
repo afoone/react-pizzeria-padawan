40b131e65a7ebc57e4a5bcb672620cb4c845f5bc
AguedaFontestad <60605015+AguedaFontestad@users.noreply.github.com>
2020-05-16 18:34:58 +0200

    boton y css

--- src/components/Ingredientes.js
+++ src/components/Ingredientes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MenuLateral from './MenuLateral'
-import './Pizza.css'
+import './Ingredientes.css'
 
 // prop pizza
 
@@ -10,7 +10,7 @@ const Ingredientes = (props) => {
     console.log(props.valor)
 
     return (
-        
+       
         <div className="ui centered card"> 
          
         <div className = "center aligned header, color">   {props.valor.name} </div>
@@ -18,7 +18,8 @@ const Ingredientes = (props) => {
          <div className ="image"> <img src={props.valor.img}></img> </div>
     
         <div className ="content, color-bajo"> {props.valor.precio} € </div>
-       </div>     
+        <button class="ui grey button"> Añadir a la Pizza</button>
+       </div>    
     
         )
 }
