c838ef058e6e63bb7c12d53457726acd198bebff
arabbuc <abarmando@gmail.com>
2020-05-18 11:34:00 +0200

    AddPizza uncionando imagen tipo text

--- src/components/AddPizza.js
+++ src/components/AddPizza.js
@@ -64,7 +64,7 @@ export class AddPizza extends Component {
           </div>
           <div class="field">
             <label>imagen</label>
-            <input type="file" name="last-name" onChange={this.onImageChange} value={this.setState.imagen} placeholder="Last Name" />
+            <input type="text" name="last-name" onChange={this.onImageChange} value={this.setState.imagen} placeholder="Last Name" />
           </div>
 
           <button class="ui button" type="submit" onClick={this.grabarPizza}>Grabar</button>
