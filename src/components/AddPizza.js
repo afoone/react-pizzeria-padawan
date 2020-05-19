3281af4ae51a9ed9d77f74b2201dcd5e4d6f199c
David Cedeño <davidcedeno@MBP-de-David.technicolor.net>
2020-05-19 13:04:11 +0200

    Pa arriba formulario validación

--- src/components/AddPizza.js
+++ src/components/AddPizza.js
@@ -2,14 +2,113 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import "./addPizza.css"
 
+
+export const MensajeError = (props) => {
+
+  if (!props.mensaje) {
+      return null;
+  }
+
+  return (
+      <div className="ui negative message">
+          <div className="header">
+              {props.mensaje}
+          </div>
+      </div>
+  )
+}
+
+
 export class AddPizza extends Component {
   constructor(props) {
     super(props)
     this.state = {
       nombre: '',
-      precio: 0,
-      imagen: ''
+      precio: '',
+      imagen: '',
+
+      nombreError: null,
+      precioError: null,
+      imagenError: null
     }
+  }
+
+  validate = () => {
+
+    let nombreError = null;
+    let precioError = null;
+    let imagenError = null;
+
+    if (this.state.nombre.length < 1) {
+        nombreError = "El nombre es obligatorio."
+
+    }
+
+    if (this.state.precio.length < 1) {
+        precioError = "El precio es obligatorio."
+
+    } else {
+
+      if (!parseInt(this.state.precio)) {
+        precioError = "El precio tiene que ser un número entero."
+    }
+
+    }
+
+    if (this.state.imagen.length <1) {
+        imagenError = "El campo es obligatorio."
+
+    }
+
+    this.setState({
+        nombreError,
+        precioError,
+        imagenError
+
+    }
+
+    )
+
+    return !nombreError && !precioError && !imagenError;
+
+  }
+
+  onSubmit = (e) => {
+    console.log("Grabando")
+
+    e.preventDefault();
+
+    if (!this.validate())
+      return;
+
+    const { nombre, precio, imagen } = this.state;
+    const nuevaPizza = {
+      nombre,
+      precio,
+      imagen
+    }
+    axios.post(' http://localhost:4000/pizzas', nuevaPizza).then(
+
+      res => {
+
+        console.log("Se grabó de forma correcta");
+
+        this.setState(
+
+          {
+
+            nombre: "",
+            precio: "",
+            imagen: ""
+
+          }
+
+        )
+
+      }
+
+    )
+
   }
   
   onNombreChange = e => {
@@ -35,40 +134,36 @@ export class AddPizza extends Component {
     /** Fin Manejadores en los inputs************ */
     }
 
-  grabarPizza = e => {
-    console.log("Grabando")
-    e.preventDefault();
-    const nuevaPizza = {
-      name: this.state.nombre,
-      precio: this.state.precio,
-      img: this.state.imagen
-    }
-    axios.post(' http://localhost:4000/pizzas', nuevaPizza).then(
-      console.log
-
-    ).catch(
-      console.error
-    )
-  }
   render() {
     return (
 
       <div className="container">
-        <form class="ui form">
-          <div class="field">
-            <label>Nombre</label>
-            <input type="text" name="first-name" onChange={this.onNombreChange} value={this.setState.nombre} placeholder="Nombre de la Pizza" />
-          </div>
-          <div class="field">
-            <label>Precio</label>
-            <input type="number" name="precio-name" onChange={this.onPrecioChange} value={this.setState.precio} placeholder="Introduce precio" />
-          </div>
-          <div class="field">
-            <label>imagen</label>
-            <input type="text" name="last-name" onChange={this.onImageChange} value={this.setState.imagen} placeholder="Introduce Imagen" />
-          </div>
 
-          <button class="ui button" type="button" onClick={this.grabarPizza}>Grabar</button>
+        <form className="ui form">
+
+              <div className={this.state.nombreError? "field error": "field"}>
+                        <label>Nombre</label>
+                        <input type="text" name="nombre"
+                            placeholder="Nombre de la pizza" value={this.state.nombre}
+                            onChange={this.onNombreChange} />
+                        <MensajeError mensaje={this.state.nombreError}></MensajeError>
+                    </div>
+                    <div className="field">
+                        <label>Precio</label>
+                        <input type="text" name="precio" placeholder="Introduce el precio"
+                            value={this.state.precio}
+                            onChange={this.onPrecioChange} />
+                        <MensajeError mensaje={this.state.precioError}></MensajeError>
+                    </div>
+                    <div className="field">
+                        <label>Imagen</label>
+                        <input type="text" name="imagen" placeholder="Carga la imagen"
+                            value={this.state.imagen}
+                            onChange={this.onImagenChange} />
+                        <MensajeError mensaje={this.state.imagenError}></MensajeError>
+                    </div>
+
+              <button class="ui button" type="submit" onClick={this.onSubmit}>Grabar</button>
         </form>
 
       </div>
