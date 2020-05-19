ef534a306a4cdb0d7d82b19f2d916adf23c26161
Alfonso Tienda <atienda@iprocuratio.com>
2020-05-19 13:06:07 +0200

    volvermos a testing

---
+++ src/components/ListaPizzas.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {jest} from '@testing-library/jest-dom'
+
+test('should render', () => {
+    beforeEach(()=>console.log("Before"))
+    describe('calls componentDidMount', () => {
+      //const spy = jes
+    })
+    
+})
