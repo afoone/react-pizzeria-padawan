7b5950e580dae30400fba205f92733dc7b49270f
David Cedeño <davidcedeno@MBP-de-David.technicolor.net>
2020-05-26 11:15:27 +0200

    LoginPage y css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Header from './components/Header'
 import ListaPizzasPage from './pages/ListaPizzasPage';
 import ListaIngredientesPage from './pages/ListaIngredientesPage'
 import CrearPizzaPage from './pages/CrearPizzaPage'
-import Login from './components/Login'
 
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
 
 
 
@@ -42,7 +42,7 @@ function App() {
 
         <Route path="/" component={Header}></Route>
         <Route exact path="/" component={ListaPizzasPage}></Route>
-        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/login" component={LoginPage}></Route>
         <Route exact path="/pizzas" component={ListaPizzasPage}></Route>
         <Route exact path= "/pizzas/add" component={CrearPizzaPage}></Route>
         <Route exact path="/ingredientes" component={ListaIngredientesPage}></Route>
