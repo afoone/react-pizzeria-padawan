a5f0e4c48e508f3727f0d326e87d8a67bfafd23c
MikiCT1980 <miguel.a.gimenezsanchez@gmail.com>
2020-05-27 13:26:59 +0200

    logout

---
+++ src/components/LogOut.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class LogOut extends Component {
+
+    constructor (props) {
+
+        super (props) ;
+        this.setState({redirigir : false})
+
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                {localStorage.removeItem("user")}
+                {this.setState ({redirigir : true})}
+                {this.state.redirigir ? <Redirect to="/pizzas"/> : null}
+                
+            </div>
+        )
+    }
+}
+
+export default LogOut
